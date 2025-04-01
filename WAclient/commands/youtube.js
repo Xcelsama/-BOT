0fc9fb8d8fcb6789874f474ba0a0cf6d042df89f
naxor <naxordeve@gmail.com>
2025-04-01 17:46:13 +0200

    Update youtube.js

--- WAclient/commands/youtube.js
+++ WAclient/commands/youtube.js
@@ -1,11 +1,21 @@
-var { Command } = require('../../lib/command'); 
-var axios = require('axios');
+const { Command } = require('../../lib/command');
+const {AddMetadata} = require('./Func/Mp3Data');
+const axios = require('axios');
 
-Command({ cmd_name: 'song', aliases: ['play'], category: 'downloader', desc: 'Download songs from YouTube' })(async (msg) => {
-  if (!msg.text) return msg.reply('Please provide a song name or YouTube link.');
-  let { data } = await axios.get(`https://downloaders-sandy.vercel.app/api/v1/yta?query=${msg.text}`);
-  if (data.status !== 'true' || !data.data) return;
-  await msg.reply(`*Downloading: ${data.data.title}...*`);
-  await msg.send({ audio: { url: data.data.downloadUrl }, mimetype: 'audio/mpeg', contextInfo: { externalAdReply: { title: data.data.title, body: 'watch on', thumbnailUrl: data.data.thumbnail, mediaType: 1, mediaUrl: `https://www.youtube.com/watch?v=${data.data.videoId}`, sourceUrl: `https://www.youtube.com/watch?v=${data.data.videoId}` } } });
+Command({
+  cmd_name: 'song',
+  aliases: ['play'],
+  category: 'downloader',
+  desc: 'Download songs from YouTube'
+})(async (msg) => {
+   if (!msg.text) return msg.reply('Send a song name or link');
+   let res = await axios.get(`https://downloaders-sandy.vercel.app/api/v1/yta?query=${msg.text}`);
+   if (res.data.status !== 'true' || !res.data.data) return;
+   let song = res.data.data;
+   let reply = await msg.reply(`*Downloading: ${song.title}...*`);
+   let audio = await AddMetadata(song.downloadUrl, song.thumbnail, { title: song.title, artist: song.uploader });
+   await msg.send({audio, mimetype: 'audio/mpeg', contextInfo: {externalAdReply: {title: song.title,body: 'watch on',thumbnailUrl: song.thumbnail,mediaType: 1, mediaUrl: `https://www.youtube.com/watch?v=${song.videoId}`,sourceUrl: `https://www.youtube.com/watch?v=${song.videoId}`
+   }
+  }});
 });
-                  
+        
