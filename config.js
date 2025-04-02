f1bffcfad550fb2f4cd93074e1131d5360fbf296
naxor <naxordeve@gmail.com>
2025-04-02 17:47:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,11 @@ module.exports = {
     GROK_API: process.env.GROK_API || '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', //get key at console.groq.com
 
     SESSION_ID: process.env.SESSION_ID || '', //session id
-
-    MONGODB_URI: process.env.MONGODB_URI || '', // get url at mongodb.atlas
     
-    PASTE_BIN_API: process.env.PASTE_BIN_API || '', //pastebin key for session 
+
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://whatsbixby:whatsbixby@cluster0.idp3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', // get url at mongodb.atlas
+    
+    PASTE_BIN_API: process.env.PASTE_BIN_API || 'ypkqXUGgzysc_yLPTBaEZ_G3G-nvjEsh', //pastebin key for session 
 
     API: 'https://diegoson-naxordeve.hf.space',
     
