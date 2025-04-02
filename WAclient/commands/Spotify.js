876caa41272c99117b42a92404439c7e4f0f78f0
naxor <naxordeve@gmail.com>
2025-04-02 13:34:04 +0200

    Update Spotify.js

--- WAclient/commands/Spotify.js
+++ WAclient/commands/Spotify.js
@@ -4,7 +4,7 @@ const config = require('../../config');
 var {AddMetadata} = require('./Func/Mp3Data');
 
 Command({
-  cmd_name: 'spotifydl',
+  cmd_name: 'spotify',
   aliases: ['spdl'],
   category: 'downloader',
   desc: 'Download songs from Spotify'
@@ -12,7 +12,7 @@ Command({
   if (!msg.text) return msg.reply('Provide a valid Spotify track url');
   let { data } = await axios.get(`${config.API}/Spotify/download?url=${msg.text}`);
   if (!data || !data.download) return;
-  let caption = `*Title:* ${data.title}\n*Artist:* ${data.artist}\n*Duration:* ${(data.duration / 1000).toFixed(0)} seconds`;
+  let caption = `*Title:* ${data.title}\n*Duration:* ${(data.duration / 1000).toFixed(0)} seconds`;
   await msg.send({ image: { url: data.cover }, caption });
   let mp3Buffer = await AddMetadata(data.download, data.cover, { 
     title: data.title, 
