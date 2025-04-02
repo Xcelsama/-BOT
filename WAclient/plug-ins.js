2fa26937ec6d14bffb096e8773cc84cfd6d75d0c
naxor <naxordeve@gmail.com>
2025-04-02 17:12:16 +0200

    Update plug-ins.js

--- WAclient/plug-ins.js
+++ WAclient/plug-ins.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function plugins() {
     const com = path.join(__dirname, 'commands');
-    const c = fs.readdirSync(com).filter(file => file.endsWith('.js'));
+    const co = fs.readdirSync(com).filter(file => file.endsWith('.js'));
     for (const file of co) {
         require(path.join(com, file));
     } console.log('Commands loaded✅');
