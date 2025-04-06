13c8a65d66f93078e77b7578333e86a46fac162c
naxor <naxordeve@gmail.com>
2025-04-06 18:16:37 +0200

    mediafire.js

---
+++ WAclient/commands/mediafire.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+var { Command } = require('../../lib/command');
+
+Command({
+    cmd_name: "mediafire",
+    aliases: ["mf", "mfire"],
+    category: "downloader",
+    desc: "Download MediaFire file"
+})(async (msg) => {
+    let url = extractUrl(msg.text);
+    if (!url && msg.quoted) {
+        url = extractUrl(
+            msg.quoted.message?.conversation || msg.quoted.message?.extendedTextMessage?.text || ""
+        );
+    }
+    if (!url || !url.startsWith("https://www.mediafire.com/")) return msg.reply("*_Please provide a valid mediafire url_*");
+    const api = `https://diegoson-naxordeve.hf.space/mediafire?url=${url}`;
+    const { data } = await axios.get(api);
+    if (data?.downloadLink) {
+        await msg.send({document: { url: data.downloadLink },fileName: data.fileName, mimetype: 'application/octet-stream', caption: `*Name:* ${data.fileName}\n*Size:* ${data.fileSize}\n*X ASTRAL*`
+        });
+    }
+});
+      
