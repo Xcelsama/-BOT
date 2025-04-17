d9bea30ab74e00db5516c2dd0c7805de5ef5563c
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-17 18:22:40 +0200

    Update welcome.js

--- WAclient/commands/welcome.js
+++ WAclient/commands/welcome.js
@@ -1,5 +1,4 @@
-
-const { Command } = require('../../lib/command');
+const {Command} = require('../../lib/command');
 const Group = require('../../lib/DB/schemas/GroupSchema');
 
 Command({
@@ -9,30 +8,22 @@ Command({
     desc: 'Toggle welcome message or set custom welcome message',
     usage: '.welcome [on/off] or .welcome <custom message>'
 })(async (msg, args) => {
-    if (!msg.isGroup) return msg.reply('*This command only works in groups!*');
+    if (!msg.isGroup) return;
     if (!msg.isAdmin && !msg.fromMe) return msg.reply('*Admin only command!*');
-
     let group = await Group.findOne({ id: msg.user }) || await new Group({ id: msg.user }).save();
-    
-    if (!args.length) {
-        group.welcome = !group.welcome;
-        await group.save();
-        return msg.reply(`*Welcome message ${group.welcome ? 'enabled' : 'disabled'}*`);
-    }
-
+    if (!args.length) { group.welcome = !group.welcome;
+     await group.save();
+     return msg.reply(`*Welcome msg ${group.welcome ? 'enabled' : 'disabled'}*`); }
     if (args[0].toLowerCase() === 'on') {
         group.welcome = true;
         await group.save();
         return msg.reply('*Welcome message enabled*');
-    }
-
-    if (args[0].toLowerCase() === 'off') {
+      }if (args[0].toLowerCase() === 'off') {
         group.welcome = false;
         await group.save();
         return msg.reply('*Welcome message disabled*');
     }
-
-    group.welcomeMsg = args.join(' ');
+    group.welcomemsg = args;
     await group.save();
-    return msg.reply(`*Welcome message updated*\n\nPreview:\n${group.welcomeMsg}`);
+    return msg.reply(`*Welcome message updated*\n\nPreview:\n${group.welcomemsg}`);
 });
