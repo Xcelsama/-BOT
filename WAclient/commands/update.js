6647d740049cad9df998c78d2e8cb3b5f916210f
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-18 18:56:50 +0200

    naxor

--- WAclient/commands/update.js
+++ WAclient/commands/update.js
@@ -9,14 +9,14 @@ Command({
      await msg.reply('*Checking updates...*');
      await git.fetch();
      const st = await git.status();
-     if (!st.behind) return msg.reply('*Bot is up to date*');
+     if (!st.behind) return msg.reply('*✅Bot is up to date*');
      const log = await git.log(['HEAD..origin/main']);
      const cl = log.all
             .map(c => `• ${c.message}`)
             .join('\n');
     
         if (msg.body.includes('now')) {
-            await msg.reply('*Updating...*');
+            await msg.reply('*🔄Updating...*');
             await git.pull();
             return msg.reply('*✓ Updated*\n_Restart to apply changes_');
         }
