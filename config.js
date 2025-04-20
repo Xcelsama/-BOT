fdf1b548b20a4569959e101eb4b0b20c1b7a815d
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-20 10:51:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     OWNER_NUM: process.env.OWNER_NUMBER || '27686881509',
     MODS: (process.env.MODS || '27686881509').split(',').filter(Boolean),
     
-    BOT_NAME: process.env.BOT_NAME || 'X-Astral',
+    BOT_NAME: process.env.BOT_NAME || 'X-ASTRAL',
     
     GROK_API: process.env.GROK_API || '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', //get key at console.groq.com
 
@@ -14,11 +14,13 @@ module.exports = {
 
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://whatsbixby:whatsbixby@cluster0.idp3t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', // get url at mongodb.atlas
     
-    PASTE_BIN_API: process.env.PASTE_BIN_API || 'ypkqXUGgzysc_yLPTBaEZ_G3G-nvjEsh', //pastebin key for session 
-
-    API: 'https://diegoson-naxordeve.hf.space',
     
-    FOOTER: process.env.FOOTER || '© wabot',
+    API: 'https://diegoson-naxordeve.hf.space',
+
+
+    XP_SYSTEM: process.env.XP_SYSTEM || '', //treu/false
+    
+    FOOTER: process.env.FOOTER || '© whatsapp',
     LANG: process.env.LANG || 'en',
     WORKTYPE: process.env.WORK_TYPE || 'private'
 };
