158bfa09052863d3c2c1fb5ab03222090931c7a8
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-20 15:21:12 +0200

    naxor

---
+++ WAclient/commands/gemini-pro.js
@@ -0,0 +1,26 @@
+const genai = require('@google/genai');
+const {Command} = require("../../lib/command");
+var config = require("../../config");
+
+Command({
+    cmd_name: "geminipro",
+    category: "AI",
+    desc: "Google gemini pro"
+})(async (msg) => {
+    if (!config.GEMINI_API_KEY) return msg.reply("*Missing GEMINI_API_KEY please provide api*");
+    if (!msg.text && !msg.quoted?.image) return msg.reply("*_Please provide a prompt_*");
+    var v = await msg.send("Processing..."); 
+        genai.configure({ apiKey: config.GEMINI_API_KEY });
+        if (msg.quoted?.image) {
+            const model = genai.getGenerativeModel({ model: "gemini-pro-vision" });
+            var img = await msg.quoted.download();
+            const result = await model.generateContent([img, msg.text || "Describe this image"]);
+            let res = await result.response;
+            await msg.send({ text: res.text(), edit: v.key });
+        } else {
+            var model = genai.getGenerativeModel({ model: "gemini-pro" });
+            const result = await model.generateContent(msg.text);
+            let res = await result.response;
+            await msg.send({ text: res.text(), edit: v.key });
+        }
+});
