ad3068c6ef763fabcfa3fbf721bffed8487198e0
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-23 21:00:23 +0200

    naxor

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const config = require('./config');
 const { startBot } = require('./lib/client');
 const { SessionCode } = require('./lib/session');
+const {connectMongoDB} = require('./lib/DB/database');
 
-async function setupSession() {
+async function Client() {
     if (!fs.existsSync("./lib/multi_auth/creds.json")) 
     await SessionCode(config.SESSION_ID || process.env.SESSION_ID, "./lib/multi_auth");
+    await connectMongoDB();
     await startBot();
 }
 
-setupSession();
+Client();
