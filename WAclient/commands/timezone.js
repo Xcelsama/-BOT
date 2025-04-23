36d189542d5f326bd891d69800e9f62800c7bd6e
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-23 13:54:44 +0200

    naxor

--- WAclient/commands/timezone.js
+++ WAclient/commands/timezone.js
@@ -1,6 +1,6 @@
 var {Command} = require('../../lib/command');
 const moment = require('moment-timezone');
-
+          
 Command({
   cmd_name: 'time',
   category: 'misic',
@@ -10,12 +10,12 @@ Command({
   if (!zone) return await msg.send('*usage:* time <Timezone>\n_Example:_ time Africa/Johhansburg');
   if (!moment.tz.zone(zone)) { return await msg.send(`*invalid timezone*\nCheck: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones`); }
   const now = moment().tz(zone);
-  const ctx = `╭── *Current Time*\n
+  const ctx = `╭──【*Current Time*】\n
 ├─*Zone:* ${zone}
 ├─*Date:* ${now.format('dddd, MMMM Do YYYY')}
 ├─*Time:* ${now.format('HH:mm:ss')}
 ├─*UTC Offset:* UTC${now.format('Z')}
-╰─────
+╰─────╼
   `.trim();
   await msg.send(ctx);
 });
