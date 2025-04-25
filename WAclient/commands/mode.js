8cba1443c410066cd090b39412d4945a3d6602fb
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-25 15:14:32 +0200

    naxor

--- WAclient/commands/mode.js
+++ WAclient/commands/mode.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { Command } = require('../../lib/command.js');
-const config = require('../../config.js');
+const config = path.join(__dirname, '../../config.js');
 
 Command({
   cmd_name: 'mode',
