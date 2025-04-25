2664123aefd55b5ff05e64fe86d1301a5f34c0eb
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-25 14:30:18 +0200

    naxor

--- WAclient/commands/vv-once.js
+++ WAclient/commands/vv-once.js
@@ -1,29 +1,29 @@
-const { Command } = require('../../lib/command');
+const {Command} = require('../../lib/command');
+var { getContentType } = require('@whiskeysockets/baileys');
 
 Command({
     cmd_name: 'vv',
     aliases: ['antivo', 'antiviewonce'],
     category: 'tools',
     desc: 'Convert view once media to normal media'
-})
-  (async (msg) => {
-    if (!msg.quoted) return msg.reply('*Reply to a view once message*');
-    if (!msg.quoted.message?.viewOnceMessageV2?.message) return;
-    const stream = await msg.quoted.download();
-    if (!stream) return;
+})(async (msg) => {
+    if (!msg.quoted) return msg.reply('*Reply to a view once message*');    
+    if (!msg.quoted.message?.viewOnceMessageV2?.message) {
+        return msg.reply('*This is not a view once*');
+    }
+    const type = getContentType(msg.quoted.message.viewOnceMessageV2.message);
+    const stream = await msg.quoted.download(); 
+    if (!stream) return msg.reply('*oops');  
     let buffer = Buffer.from([]);
     for await (const chunk of stream) {
         buffer = Buffer.concat([buffer, chunk]);
     }
-    
-    const type = msg.quoted.type === 'viewOnceMessageV2' 
-    ? Object.keys(msg.quoted.message.viewOnceMessageV2.message)[0]
-   : msg.quoted.type;
-    const ctx = msg.quoted.message.viewOnceMessageV2.message[type]?.caption || '';
-    const cap = `*HERE IS YOUR*\n\n${ctx}`;
+
+    const caption = msg.quoted.message.viewOnceMessageV2.message[type]?.caption || '';
     if (type.includes('image')) {
-        await msg.send({ image: buffer, caption: cap });
+        await msg.send({ image: buffer, caption});
     } else if (type.includes('video')) {
-        await msg.send({ video: buffer, caption: cap });
+        await msg.send({ video: buffer, caption});
     }
 });
+        
