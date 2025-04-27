6c6cf545fa714720bebccc562a7e536faabb630c
naxor <naxordeve@gmail.com>
2025-04-27 23:06:53 +0200

    naxor

--- WAclient/commands/fullpp.js
+++ WAclient/commands/fullpp.js
@@ -1,27 +1,14 @@
-const { Command } = require('../../lib/command');
+const {Command} = require('../../lib/command');
 
 Command({
-    cmd_name: "fullpp",
-    aliases: ["fp"],
-    category: "owner",
-    desc: "Set full profile picture"
-})
-(async ( msg ) => {
-        if (!msg.fromMe) return;
-        let image;
-        if (msg.quoted && msg.quoted.type.includes('image')) {
-            const buffer = await msg.quoted.download();
-            image = buffer;
-        } else if (msg.type.includes('image')) {
-            const buffer = await msg.download();
-            image = buffer;
-        } else {
-            return msg.reply('*_Please reply to an image_*');
-        }
-        var success = await msg.toFullpp(image);
-        if (success) {
-            await msg.reply('*_Profile picture updated_*');
-        } else {
-            await msg.reply('_err_');
-        }
+    cmd_name: 'fullpp',
+    aliases: ['setp'],
+    category: 'owner',
+    desc: 'Set full size profile picture'
+})(async (msg) => {
+    if (!msg.fromMe) return;
+    if (!msg.quoted || !msg.quoted.message) return msg.reply('*_Reply to an image_*');
+    const buffer = await msg.quoted.download();
+    await msg.updateProfilePicture(buffer);
+    await msg.reply('*_Profile  updated_*');
 });
