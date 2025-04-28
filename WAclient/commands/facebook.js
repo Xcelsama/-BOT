a5ace3c792046533d28039135af4556ec32e7ebb
naxor <naxordeve@gmail.com>
2025-04-28 14:53:44 +0200

    naxor

--- WAclient/commands/facebook.js
+++ WAclient/commands/facebook.js
@@ -14,7 +14,8 @@ Command({
       msg.quoted.message?.conversation || 
       msg.quoted.message?.extendedTextMessage?.text || ''
     );
-  } if (!url) return msg.reply('Please provide a facebook url');
+  } 
+  if (!url) return msg.reply('Please provide a facebook url');
   let { data } = await axios.get(`${config.API}/facebook?url=${url}`);
   if (data.status !== 200 || !data.data) return;
   let vid = data.data['720p (HD)'] || data.data['360p (SD)'];
