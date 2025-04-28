0e592de4d26106b227ce1462a155cdd7f7ff1aed
naxor <naxordeve@gmail.com>
2025-04-28 14:39:01 +0200

    Update facebook.js

--- WAclient/commands/facebook.js
+++ WAclient/commands/facebook.js
@@ -8,10 +8,17 @@ Command({
   category: 'downloader',
   desc: 'Download Facebook videos'
 })(async (msg) => {
-  if (!msg.text) return msg.reply('Please provide a fb url');
-  let { data } = await axios.get(`${config.API}/facebook?url=${msg.text}`);
+  if (!msg.text) return msg.reply('Please provide a facebook url');
+  let url = extractUrl(msg.text);
+  if (!url && msg.quoted) {
+    url = extractUrl(
+      msg.quoted.message?.conversation || 
+      msg.quoted.message?.extendedTextMessage?.text || ''
+    );
+  }
+  let { data } = await axios.get(`${config.API}/facebook?url=${url}`);
   if (data.status !== 200 || !data.data) return;
   let vid = data.data['720p (HD)'] || data.data['360p (SD)'];
   if (!vid) return;
-  await msg.send({video: { url: vid }, caption: `*▢Quality:* ${data.data['720p (HD)'] ? 'HD (720p)' : 'SD (360p)'}\n*▢X ASTRAL*`});
+  await msg.send({video: { url: vid }, caption: `*▢Quality:* ${data.data['720p (HD)'] ? 'HD (720p)' : 'SD (360p)'}\n*▢Made with❤️*`});
 });
