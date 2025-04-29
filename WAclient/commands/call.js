d36e6fb6f5493a760419cf8f74ee1e6e40efb1f1
naxor <naxordeve@gmail.com>
2025-04-29 20:45:39 +0200

    naxor

--- WAclient/commands/call.js
+++ WAclient/commands/call.js
@@ -1,21 +1,17 @@
-
 const Call = require('../../lib/DB/schemas/CallSchema');
 const { Command } = require('../../lib/command');
 
 Command({
     cmd_name: 'call',
-    category: 'admin',
+    category: 'owner',
     desc: 'Toggle global call auto-reject',
     usage: '.call on/off'
 })(async (msg) => {
-    if (!msg.fromMe && !msg.isSudo) return msg.reply('Only bot owner can use this command');
-    
+    if (!msg.fromMe) return;
     const action = msg.text.toLowerCase();
     if (!['on', 'off'].includes(action)) return msg.reply('Use on or off');
-    
     const settings = await Call.findOne({}) || await new Call({}).save();
     settings.globalAutoReject = action === 'on';
     await settings.save();
-    
-    return msg.reply(`Global call auto-reject has been ${action === 'on' ? 'enabled' : 'disabled'}`);
+    return msg.reply(`auto-call has been ${action === 'on' ? 'enabled' : 'disabled'}`);
 });
