f3d77509ca34f2bf159509fbc63468223a53bb57
naxor <naxordeve@gmail.com>
2025-05-03 14:41:39 +0200

    Update

--- WAclient/commands/formater.js
+++ WAclient/commands/formater.js
@@ -3,20 +3,23 @@ const { Sticker } = require('wa-sticker-formatter');
 
 Command({
     cmd_name: 'take',
-    category: 'converter',
-    desc: 'Convert media to sticker with given crop'
+    category: 'tools',
+    desc: 'Convert'
 })(async (msg, conn) => {
     const quoted = msg.quoted ? msg.quoted : msg;
     const mime = quoted.type || '';
     if (!/image|video|gif/.test(mime)) return msg.reply('_Reply to an image, video, or GIF_');
+    const [pack, author] = msg.text.split(',').map(x => x.trim());
+    if (!pack || !author) return msg.reply('_usage:_ take Packname, Author');
     const media = await quoted.download();
     const sticker = new Sticker(media, {
-    pack: 'Created By', author: 'Bot',
-    type: 'full', quality: 70,
+    pack, author,type: 'full',quality: 70,
     background: 'transparent'
     });
+
     await msg.send({ sticker: await sticker.toBuffer() });
 });
+
 
 Command({
     cmd_name: 'crop',
