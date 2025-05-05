20142d35c32514aa005a50d55cca6d8939ecc901
naxor <naxordeve@gmail.com>
2025-05-05 18:53:04 +0200

    naxor

--- WAclient/commands/group.js
+++ WAclient/commands/group.js
@@ -1,5 +1,44 @@
 var { Command } = require('../../lib/command');
 var { extractUrl } = require('../../lib/Functions');
+const Group = require('../../lib/models/schemas/GroupSchema');
+
+Command({
+  cmd_name: 'welcome',
+  category: 'admin',
+  desc: 'Toggle welcome message on/off',
+  usage: '.welcome [on/off]'
+})(async (msg) => {
+  if (!msg.isGroup) return;
+  if (!msg.isAdmin && !msg.fromMe) return;
+  const args = msg.text.toLowerCase();
+  if (!['on', 'off'].includes(args)) return msg.reply('Use on or off');
+  let group = await Group.findOne({ id: msg.user }) || await new Group({ id: msg.user }).save();
+  if (args === 'on') {
+    group.welcome = true;
+    await group.save();
+    return msg.reply('*Welcome message enabled*');
+  } if (args === 'off') {
+    group.welcome = false;
+    await group.save();
+    return msg.reply('*Welcome message disabled*');
+  }
+});
+
+Command({
+    cmd_name: 'setwelcome',
+    category: 'admin',
+    desc: 'Set custom welcome message',
+    usage: '.setwelcome <custom message>'
+})(async (msg) => {
+    if (!msg.isGroup) return;
+    if (!msg.isAdmin && !msg.fromMe) return;
+    var args = msg.text;
+    if (!args.length) return msg.reply('*Please provide a welcome message*');
+    let group = await Group.findOne({ id: msg.user }) || await new Group({ id: msg.user }).save();
+    group.welcomemsg = args;
+    await group.save();
+    return msg.reply(`*Welcome message updated*\n\nPreview:\n${group.welcomemsg}`);
+});
 
 Command({
   cmd_name: 'mentionall',
