9c7623fa12b59919f081cac844eb553162bcd004
naxor <naxordeve@gmail.com>
2025-05-05 19:28:26 +0200

    naxor

---
+++ WAclient/commands/autodl.js
@@ -0,0 +1,22 @@
+var { extractUrl, igdl } = require('../../lib/Functions');
+var { Command } = require('../../lib/command');
+
+Command({
+  on: 'text'
+})(async (msg) => {
+  const url = extractUrl(msg.body);
+  if (!url || !url.includes('instagram.com')) return;
+  let res;
+  try { res = await igdl(url);
+  } catch (e) {
+  return msg.reply('yh neh');
+  } if (!res || !res.links.length) return;
+  for (const link of res.links) {
+    if (res.type === 'video') {
+      await msg.send({ video: { url: link }, caption: 'Video❤️' });
+    } else {
+      await msg.send({ image: { url: link }, caption: 'Photo❤️' });
+    }
+  }
+});
+      
