17b9c6787f775dff12bd16348c45544bf52ceabb
naxor <naxordeve@gmail.com>
2025-05-10 21:12:56 +0200

    Update

--- WAclient/commands/sticker.js
+++ WAclient/commands/sticker.js
@@ -1,22 +1,28 @@
-const { Command } = require('../../lib/command');
+const { Command } = require('../../lib/');
 const { makeSticker } = require('../../lib/Sticker');
-const config = require('../../config'); 
+const fs = require('fs');
+var config = require('../../config');
 
 Command({
     cmd_name: 'sticker',
+    aliases: ['s'],
     category: 'converter',
-    desc: 'Convert image/video to sticker'
-})(async (msg, conn) => {
-    const quoted = msg.quoted ? msg.quoted : msg;
-    const mime = quoted.type || '';
-    if (!/image|video|gif/.test(mime)) return msg.reply('_Reply to an image, video, or GIF_');
-    msg.reply('Creating stk...');
-    const mediaData = await quoted.download();
-    let buffer = Buffer.from([]);
-    for await (const chunk of mediaData) {
-    buffer = Buffer.concat([buffer, chunk]);}
-    const sticker = await makeSticker(buffer, mime);
-    if (!sticker) return msg.reply('err');
-    await msg.send({sticker });
+    desc: 'Convert media to sticker'
+})(async (msg) => {
+        const buffer = msg.type === 'imageMessage' || msg.type === 'videoMessage' 
+            ? await msg.download() 
+            : msg.quoted?.type === 'imageMessage' || msg.quoted?.type === 'videoMessage'
+            ? await msg.quoted.download()
+            : null;
+
+        if (!buffer) return msg.reply('Reply to an image/video');
+        const stk = await makeSticker(buffer, {
+            pack: config.PACKNAME
+        });
+
+        await msg.send({ 
+            sticker: fs.readFileSync(stk)
+        });
+        
+        fs.unlinkSync(stk);
 });
-    
