9da683eaff38b681e7dae49c01032b7dd8ab1892
naxor <naxordeve@gmail.com>
2025-05-13 15:44:35 +0200

    naxor

--- WAclient/commands/formater.js
+++ WAclient/commands/formater.js
@@ -1,116 +1,95 @@
-const { Command } = require('../../lib/command');
-const { Sticker } = require('wa-sticker-formatter');
-var config = require('../../config');
-const fetch = require('node-fetch');
+const { Command } = require('../../lib/');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
 
-
-Command({
-    cmd_name: 'take',
-    category: 'tools',
-    desc: 'Convert',
-})(async (msg) => {
-        if (!msg.quoted || !msg.quoted.message) return await msg.reply('_Reply to a sticker_');
-        var match = msg.text;
-        let pack = config.PACKNAME;
-        let author = 'NᴀxᴏʀDᴇᴠɪ';
-        if (match) { if (!match.includes('|')) return await msg.reply('_use the correct format: `packname|author`_');
-        let [ux, us] = match.split('|').map((v) => v.trim());
-        if (!ux || !us) return await msg.reply('_Both packname and author must be provided in the format: `packname|author`_');
-        pack = ux;
-        author = us; }
-        const buffer = await msg.quoted.download();
-        if (!buffer) return;
-        const sticker = new Sticker(buffer, {
-        pack,author,type: 'full',quality: 80,id: 'sticker_cmd',background: 'transparent'
-        });
-        const voidi = await sticker.toBuffer();
-        await msg.send({ sticker: voidi });
-});
-
-    
-
-Command({
-    cmd_name: 'crop',
-    category: 'converter',
-    desc: 'Convert media to cropped sticker'
-})(async (msg, conn) => {
-    if (!msg.quoted) return msg.reply('_Reply to an sticker_');
-    const media = await msg.quoted.download();
-    const sticker = new Sticker(media, {
-    pack: 'Created By', author: 'Xastral',
-    type: 'crop',quality: 70,
-    background: 'transparent'
+async function upscale(filePath) {
+    const buffer = fs.readFileSync(filePath);
+    const ext = path.extname(filePath).slice(1) || 'bin';
+    const mime = ext === 'png' ? 'image/png' : ext === 'jpg' || ext === 'jpeg' ? 'image/jpeg' : 'application/octet-stream';
+    const fn = Math.random().toString(36).slice(2, 8) + '.' + ext;
+    const { data } = await axios.post("https://pxpic.com/getSignedUrl", {
+        folder: "uploads",
+        fileName
+    }, {
+        headers: {
+            "Content-Type": "application/json"
+        }
     });
-    await msg.send({ sticker: await sticker.toBuffer() });
-});
 
-Command({
-    cmd_name: 'circle',
-    category: 'converter',
-    desc: 'Convert media to circular sticker'
-})(async (msg, conn) => {
-    if (!msg.quoted) return msg.reply('_Reply to an sticker_');
-    const media = await msg.quoted.download();
-    const sticker = new Sticker(media, {
-    pack: `${config.PACKNAME}`, author: `${msg.pushName}`,
-    type: 'circle', quality: 70,
-    background: 'transparent'
+    await axios.put(data.presignedUrl, buffer, {
+        headers: {
+            "Content-Type": mime
+        }
     });
-    await msg.send({ sticker: await sticker.toBuffer() });
-});
-
-
-async function removebg(buffer) {
-    return new Promise(async (resolve, reject) => {
-        const image = buffer.toString("base64");
-        let res = await axios.post(
-        "https://us-central1-ai-apps-prod.cloudfunctions.net/restorePhoto", {
-        image: `data:image/png;base64,${image}`,
-        model: "fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003",},);
-        const data = res.data?.replace(`"`, "");
-        if (!data) return reject("err");
-        resolve(data);
-    });
+    const url = "https://files.fotoenhancer.com/uploads/" + fn;
+    const api = await (await axios.post("https://pxpic.com/callAiFunction", new URLSearchParams({
+        imageUrl: url,
+        targetFormat: 'png',
+        needCompress: 'no',
+        imageQuality: '100',
+        compressLevel: '6',
+        fileOriginalExtension: 'png',
+        aiFunction: 'upscale',
+        upscalingLevel: ''
+    }).toString(), {
+        headers: {
+        'User-Agent': 'Mozilla/5.0 (Android 10; Mobile; rv:131.0) Gecko/131.0 Firefox/131.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'accept-language': 'id-ID'
+        }
+    }).catch(e => e.response)).data;
+    const buffersize = await (await axios.get(api.resultImageUrl, {
+    responseType: 'arraybuffer'
+    }).catch(e => e.response)).data;
+    return {
+        status: 200,
+        success: true,
+        result: {
+        size: formatSize(buffer.length),
+        imageUrl: api.resultImageUrl
+        }
+    };
 }
 
-Command({
-    cmd_name: 'removebg',
-    category: 'tools',
-    desc: 'Removes the background from an image',
-})(async (msg) => {
-        if (!msg.quoted || !msg.quoted.message.imageMessage) return await msg.reply('_Reply to an image_');
-        const buffer = await msg.quoted.download();
-        const voidi = await removebg(buffer).catch(err => null);
-        if (!voidi || typeof voidi !== "string") return;
-        const o = await axios.get(voidi, { responseType: 'arraybuffer' });
-        const x = Buffer.from(o.data);
-        await msg.send({ image: x, caption: '_Made with❤️_' });
-    
-});
-
-async function Upscale(img) {
-    const res = await fetch("https://lexica.qewertyy.dev/upscale", {
-        body: JSON.stringify({
-        image_data: Buffer.from(img, "base64"),
-        format: "binary",
-        }),
-        headers: {"Content-Type": "application/json",},
-        method: "POST",
-    });
-    if (!res.ok) return null;
-    return Buffer.from(await res.arrayBuffer());
+function formatSize(size) {
+    function round(value, precision) {
+        var multiplier = Math.pow(10, precision || 0);
+        return Math.round(value * multiplier) / multiplier;
+    }
+    var kiloByte = 1024;
+    var megaByte = kiloByte * kiloByte;
+    var gigaByte = kiloByte * megaByte;
+    var teraByte = kiloByte * gigaByte;
+    if (size < kiloByte) {
+        return size + "B";
+    } else if (size < megaByte) {
+        return round(size / kiloByte, 1) + "KB";
+    } else if (size < gigaByte) {
+        return round(size / megaByte, 1) + "MB";
+    } else if (size < teraByte) {
+        return round(size / gigaByte, 1) + "GB";
+    } else {
+        return round(size / teraByte, 1) + "TB";
+    }
 }
 
 Command({
     cmd_name: 'upscale',
     aliases: ['hd'],
     category: 'tools',
-    desc: 'Upscales an image to higher resolution',
-    })(async (msg) => {
-    if (!msg.quoted || !msg.quoted.message.imageMessage) return await msg.reply('_Reply to an image_');
-    const buffer = await msg.quoted.download();
-    const ups = await Upscale(buffer).catch(() => null);
-    if (!ups) return;
-    await smg.send({ image: ups, caption: '_upscaled_' });
+    desc: 'Upscale an image using AI'
+})(async (msg) => {
+    if (!msg.quoted || !msg.quoted.message || !msg.quoted.message.imageMessage) {
+    return msg.reply('_Reply to an image_');
+    }   const buffer = await msg.quoted.download();
+    const tm = path.join(__dirname, `../remove/${Math.random().toString(36).slice(2)}.png`);
+    fs.writeFileSync(tm, buffer);
+    const result = await upscale(tm);
+    fs.unlinkSync(tm);
+    if (result.success) {
+    await msg.send({image: { url: result.result.imageUrl }, caption: `*4KHD*\n*Size:* ${result.result.size}`
+    });
+ } 
 });
-    
