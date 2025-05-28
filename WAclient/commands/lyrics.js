9aa05622392cb6422008f666588ad4ef1b1c46a5
naxor <naxordeve@gmail.com>
2025-05-28 23:53:37 +0200

    Update

--- WAclient/commands/lyrics.js
+++ WAclient/commands/lyrics.js
@@ -1,6 +1,4 @@
-var { Command } = require('../../lib/');
-const Genius = require("genius-lyrics");
-var Client = new Genius.Client("jRks96Rxyfx5U4WkMpmlafYjczSsFVovfq2P-HvbtwLN25JuvNPbF43Sz3xd7d8s");
+var {Command} = require('../../lib/');
 
 Command({
   cmd_name: 'lyrics',
@@ -8,11 +6,11 @@ Command({
   desc: 'Get lyrics for a song'
 })(async (msg) => {
   let args = msg.text;
-  if (!args) return msg.reply('Please provide a song name or query, e.g., `.lyrics Dior by Pop Smoke`');
-  var find = await Client.songs.search(args);
-  var x = find[0];
-  var ctx = x._raw.header_image_url
-  var title = x._raw.primary_artist_names + " - " + x._raw.title
-  var lyrics = await x.lyrics();
-  await msg.send({ image:{ url:ctx},caption: title + "\n\n" + lyrics });
- });
+    if (!args) return msg.reply('Please provide a song name or query, e.g., `.lyrics Dior by Pop Smoke`');
+    const result = await msg.Genius(args);
+    if (!result.ok) return;
+    const { title, artist, cover, lyrics, url } = result;
+    const caption = `*Title:* ${title}\n*Artist:* ${artist}\n\n${lyrics}\n`;
+    await msg.send({ image: { url: cover }, caption: caption });
+});
+  
