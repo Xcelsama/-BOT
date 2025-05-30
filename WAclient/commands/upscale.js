7cb4496cc3408c53a9dbccf0e27f79c58d757eba
naxor <naxordeve@gmail.com>
2025-05-30 16:44:11 +0200

    naxor

---
+++ WAclient/commands/upscale.js
@@ -0,0 +1,27 @@
+var { Command } = require('../../lib/');
+const { extractUrl } = require('../../lib/Functions');
+const axios = require('axios');
+
+Command({
+  cmd_name: 'upscale',
+  aliases: ['enhance', 'hd'],
+  category: 'tools',
+  desc: 'Upscale/enhance image quality using AI'
+})(async (msg) => {
+  let img = extractUrl(msg.text);
+  if (!img && msg.quoted) {
+    if (msg.quoted.message?.imageMessage) {
+    return msg.reply('_Please provide a direct img url_');
+    } else {
+      const q = msg.quoted.message?.conversation || 
+      msg.quoted.message?.extendedTextMessage?.text || '';
+      img = extractUrl(q);
+    }
+  } if (!img) return msg.reply('_Please reply with an image_');
+    const x = await axios.get(`https://flowfalcon.dpdns.org/imagecreator/upscale?url=${img}`);
+    if (x.data && x.data.status && x.data.result) {
+    await msg.send({ image: { url: x.data.result },
+    caption: `*HD*`
+    });
+}});
+   
