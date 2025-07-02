1ffd3147aeaedc7327b216b110a35e21387cca28
naxor <naxordeve@gmail.com>
2025-07-02 11:37:45 +0200

    naxor

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const fs = require('fs');
 const http = require('http');
+const { bot } = require('./lib/main');
+const { SessionCode } = require('./lib/session');
+const { connectDB } = require('./lib/database');
 const config = require('./config');
-const { startBot, connectMongoDB, SessionCode } = require('./lib/index');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Server Running\n');
-});
-var PORT = config.PORT || process.env.PORT;
+const startServer = () => {
+    const server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('running');
+    });
+
+    server.listen(config.PORT, () => {
+        console.log(`server: ${config.PORT}`);
+    });
+};
+
 const Client = async () => {
     try {
         await SessionCode(config.SESSION_ID || process.env.SESSION_ID, "./lib/multi_auth");
-        await connectMongoDB();
-        await startBot();
-        server.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
+        await connectDB();
+        
+        startServer();
+        console.log('Starting...');
+        await bot.start();
     } catch (error) {
-        console.error(error.stack);
-        process.exit(1); 
+        console.error(error);
     }
-}
+};
 
 Client();
