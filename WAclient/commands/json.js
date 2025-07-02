15ddd96237ffaf44247015c3564fc3489009cf65
naxor <naxordeve@gmail.com>
2025-07-02 12:20:42 +0200

    Delete WAclient directory

--- WAclient/commands/json.js
+++
@@ -1,9 +0,0 @@
-var {Command} = require('../../lib/command');
-
-Command({
-  cmd_name: 'json',
-  category: 'tools',
-  desc: 'View raw JSON of a message'
-})(async (msg) => {
-  await msg.send('```json\n' + JSON.stringify(msg, null, 2).slice(0, 4000) + '\n```');
-});
