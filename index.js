2fae14d7455b5b78819630437b5d1f777ebb4989
naxor <naxordeve@gmail.com>
2025-07-05 12:11:40 +0200

    naxor

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const { SessionCode } = require('./lib/session');
 const { connect } = require('./lib/bot');
 const { startServer } = require('./lib/server');
 
+const Client = async () => {
+    try {
+        await SessionCode(config.session_id || process.env.SESSION_ID, './lib/Session');
+        await startServer();
+        await connect();
+    } catch (error) {
+        console.error(error.stack || error);
+        process.exit(1);
+    }
+};
 
-
-startServer();
-
-connect();
+Client();
