7ce4df991539c1bce459e6b0d32d41c93826c46d
naxor <naxordeve@gmail.com>
2025-07-10 13:00:34 +0200

    naxor

--- plugins/github.js
+++ plugins/github.js
@@ -8,14 +8,14 @@ Module({
 })(async (message, match) => {
   const regex = /(?:https?:\/\/|git@)github\.com[\/:]([^\/\s]+)\/([^\/\s]+)(?:\.git)?/;
   const ctx = regex.exec(match);
-  if (!ctx) return message.reply('_Please provide a valid GitHub repo url_');
+  if (!ctx) return message.send('_Please provide a valid GitHub repo url_');
 
   const [_, username, repoRaw] = match;
   const repo = repoRaw.replace(/\.git$/, '');
 
   const api = `https://api.github.com/repos/${username}/${repo}`;
   const res = await axios.get(api).catch(() => null);
-  if (!res || res.status !== 200) return message.reply('_not found_');
+  if (!res || res.status !== 200) return message.send('_not found_');
 
   const branch = res.data.default_branch || 'main';
   const zip_url = `https://github.com/${username}/${repo}/archive/refs/heads/${branch}.zip`;
