aac1bcd3ebae60f72b7176608ac45c3e69d16bdb
naxor <naxordeve@gmail.com>
2025-07-10 20:49:12 +0200

    naxor

---
+++ plugins/sticker.js
@@ -0,0 +1,19 @@
+const { Module } = require('../lib/plugins');
+const sticker = require('../lib/sticker');
+const config = require('../config');
+
+Module({
+  command: 'sticker',
+  package: 'media',
+  description: 'Convert stk'
+})(async (message) => {
+  let mediaa = message.quoted || message;
+  if (!/image|video/.test(mediaa.type)) {
+  return await message.send('_Reply to an image or video_');}
+  const media = await mediaa.download();
+  const buffer = await sticker.toSticker(mediaa.type, media, {
+    packname: config.packname,
+    author: config.author
+  });
+  await message.send(buffer, { mimetype: 'image/webp' });
+});
