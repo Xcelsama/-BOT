7242fc27ee30e3f8157727d4396f8282fc52b576
naxor <naxordeve@gmail.com>
2025-07-10 21:53:38 +0200

    naxor

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -3,6 +3,28 @@ const sticker = require('../lib/sticker');
 const config = require('../config');
 
 Module({
+  command: 'take',
+  package: 'media',
+  description: 'change packname/author'
+})(async (message, match) => {
+  let mediaa = message.quoted || message;
+  if (mediaa.type !== 'sticker') return await message.send('_Reply to a sticker_');
+  let packname = config.packname;
+  let author = config.author;
+  if (match && match.includes('|')) {
+    const parts = match.split('|');
+    packname = parts[0].trim() || config.packname;
+    author = parts[1].trim() || config.author;
+  } const media = await mediaa.download();
+  const buffer = await sticker.toSticker('image', media, {
+    packname: packname,
+    author: author
+  });
+
+  await message.send({ sticker: buffer });
+});
+
+Module({
   command: 'sticker',
   package: 'media',
   description: 'Convert stk'
