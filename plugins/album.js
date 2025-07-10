a5285248492f549f740da1e56e58cb48f8a8b171
naxor <naxordeve@gmail.com>
2025-07-10 06:54:27 +0200

    naxor

--- plugins/album.js
+++ plugins/album.js
@@ -3,14 +3,14 @@ const SongFinder = require('../lib/Class/class');
 
 Module({
   command: 'album',
-  category: 'downloader',
+  package: 'downloader',
   description: 'Download a song pack',
 })(async (message, match) => {
   if (!match) return await message.send('_Provide album name_');
   const finder = new SongFinder();
   const result = await finder.main(match);
   if (!result?.zip) return await message.send('not found');
-  const caption = `*${result.title}*\n_Album_`;
-  await message.send({image: { url: result.image },caption });
+  const caption = `*Downloading: ${result.title} album*`;
+  await message.send(caption );
   await message.send({ document: { url: result.zip },mimetype: 'application/zip', fileName: `${result.title}.zip` });
 });
