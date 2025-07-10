3b0a8ed88c777a060c0d4594fe5fe3211e9a16f8
naxor <naxordeve@gmail.com>
2025-07-10 12:44:43 +0200

    naxor

---
+++ plugins/github.js
@@ -0,0 +1,24 @@
+const { Module } = require('../lib/plugins');
+const axios = require('axios');
+
+Module({
+  command: 'git',
+  package: 'downloader',
+  description: 'Download GitHub repo zip',
+})(async (message, match) => {
+  const regex = /(?:https?:\/\/|git@)github\.com[\/:]([^\/\s]+)\/([^\/\s]+)(?:\.git)?/;
+  const ctx = regex.exec(match);
+  if (!ctx) return message.reply('_Please provide a valid GitHub repo url_');
+
+  const [_, username, repoRaw] = match;
+  const repo = repoRaw.replace(/\.git$/, '');
+
+  const api = `https://api.github.com/repos/${username}/${repo}`;
+  const res = await axios.get(api).catch(() => null);
+  if (!res || res.status !== 200) return message.reply('_not found_');
+
+  const branch = res.data.default_branch || 'main';
+  const zip_url = `https://github.com/${username}/${repo}/archive/refs/heads/${branch}.zip`;
+  await message.send({document: { url: zip_url }, mimetype: 'application/zip', fileName: `${repo}-${branch}.zip`
+  });
+});
