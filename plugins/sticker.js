8fada160ccf730d10de370e36d69e716ddcb1881
naxor <naxordeve@gmail.com>
2025-07-11 08:50:47 +0200

    naxor

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -5,21 +5,20 @@ const config = require('../config');
 Module({
   command: 'take',
   package: 'media',
-  description: 'custom packname and author'
+  description: 'Change sticker packname and author'
 })(async (message, match) => {
   let mediaa = message.quoted || message;
-  if (!/image|video/.test(mediaa.type)) {
-  return await message.send('_Reply to an image or video_'); }
+  if (mediaa.type !== 'stickerMessage') eturn await message.send('_Reply to a sticker_');
   const [packname, author] = match?.split('|').map(s => s.trim()) || [];
-  if (!packname || !author) return await message.send('_use: take pack | author_');
+  if (!packname || !author) return await message.send('_use: take new pack | new author_');
   const media = await mediaa.download();
-  const buffer = await sticker.toSticker(mediaa.type, media, {
+  const buffer = await sticker.addExif(media, {
     packname,
     author
   });
+
   await message.send({ sticker: buffer });
 });
-
 
 Module({
   command: 'sticker',
@@ -27,7 +26,7 @@ Module({
   description: 'Convert stk'
 })(async (message) => {
   let mediaa = message.quoted || message;
-  if (!/image|video/.test(mediaa.type)) {
+  if (!/image|video|gif/.test(mediaa.type)) {
   return await message.send('_Reply to an image or video_'); }
   const media = await mediaa.download();
   const buffer = await sticker.toSticker(mediaa.type, media, {
