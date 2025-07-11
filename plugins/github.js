188d8c77a57b595bc0a63aaa4d96cdaa63a109c5
naxor <naxordeve@gmail.com>
2025-07-11 09:19:01 +0200

    naxor

--- plugins/github.js
+++ plugins/github.js
@@ -16,3 +16,27 @@ Module({
     const { name, stargazers_count, forks_count } = res.data;
     await message.send({document: { url: `${zip_url}/zipball` }, fileName: `${repo}.zip`, mimetype: "application/zip"});
 }); 
+
+Module({
+  command: 'github',
+  package: 'stalk',
+  description: 'GitHub profile info'
+})(async (message, match) => {
+  const username = match?.trim();
+  if (!username) return await message.send('_github <username>_');
+  const res = await axios.get(`https://toxicdevilapi.vercel.app/stalk/github_user?username=${username}`);
+  const data = res.data?.result;
+  if (!data) return await message.send('_not found_');
+  const avatar = (await axios.get(data.pp, { responseType: 'arraybuffer' })).data;
+  const text =`Name: ${data.name}\n` +
+    `User: ${data.username.github}\n` +
+    `Bio: ${data.bio || '-'}\n` +
+    `Location: ${data.location || '-'}\n` +
+    `Repos: ${data.ropos_count} | Gists: ${data.gists_count}\n` +
+    `Followers: ${data.followers_count} | Following: ${data.following_count}\n` +
+    `Hireable: ${data.hireable ? 'Yes' : 'No'}\n` +
+    `Created: ${data.created}\n` +
+    `Updated: ${data.last_profile_updated}\n` +
+    `Link: ${data.profileUrl}`;
+  await message.send({ image: avatar, caption: text });
+});
