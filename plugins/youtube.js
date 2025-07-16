bd7f84ad3395fa36deb3898a72360dbc7bcc3300
naxor <naxordeve@gmail.com>
2025-07-16 17:14:24 +0200

    naxor

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,49 +1,22 @@
+const yt = require('yt-search');
+const ytdl = require('../lib/download');
 const { Module } = require('../lib/plugins');
-const Tube = require('../lib/Class/Tube'); 
-const MetadataEditor = require('../lib/Class/metadata'); 
 
 Module({
   command: 'song',
   package: 'downloader',
   description: 'Search and download a song',
 })(async (message, match) => {
-  if (!match) return await message.send('_Please enter a song name_');
-  const tube = new Tube();
-  const results = await tube.find(match);
-  if (!results || results.length === 0) return;
-  const video = results.find(v => v.url) || results[0];
-  if (!video.url) return await message.send('not valid_');
-  await message.send(`\`\`\`Downloading: ${video.title}\`\`\``);
-  const dlResult = await tube.dl(video.url, 'mp3');
-  if (dlResult.error || !dlResult.url) return await message.send('_');
-  let dl_audio;
-  var editor = new MetadataEditor();
-  dl_audio = await editor.write(dlResult.url, dlResult.thumb, {
-      title: dlResult.title || video.title
-  });
-  
-  await message.send({document: dl_audio, mimetype: 'audio/mpeg', fileName: `${dlResult.title || video.title}.mp3`});
-});
+  if (!match) return message.send('Please provide a song name or yt url');
+  let link = match;
+  if (!/youtu\.be|youtube\.com/.test(match)) {
+  const res = await yt(match);
+  if (!res.videos.length) return message.send('_not found_');
+  link = res.videos[0].url;
+  }
 
-Module({
-  command: 'play',
-  package: 'downloader',
-  description: 'Search and download a song from YouTube',
-})(async (message, match) => {
-  if (!match) return await message.send('_Please enter a song name_');
-  const tube = new Tube();
-  const results = await tube.find(match);
-  if (!results || results.length === 0) return await message.send('_not found_');
-  const video = results.find(v => v.url) || results[0];
-  if (!video.url) return await message.send('_');
-  await message.send(`\`\`\`Downloading: ${video.title}\`\`\``);
-  const dlResult = await tube.dl(video.url, 'mp3');
-  if (dlResult.error || !dlResult.url) return;
-  let dl_audio;
-  var editor = new MetadataEditor();
-  dl_audio = await editor.write(dlResult.url, dlResult.thumb, {
-      title: dlResult.title || video.title
-  });
-  await message.send({audio: dl_audio, mimetype: 'audio/mpeg', fileName: `${dlResult.title || video.title}.mp3`
-  });
+  const save = new ytdl();
+  const res = await save.down(link, 'mp3');
+  if (!res.status) return message.send(res.error);
+  await message.send({ audio: { url: res.result.download }, mimetype: 'audio/mpeg' }, { quoted: message });
 });
