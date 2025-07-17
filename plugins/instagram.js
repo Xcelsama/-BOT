b9f417b7d0801fac21f83b8f630475e8f6d9d826
naxor <naxordeve@gmail.com>
2025-07-17 08:25:30 +0200

    naxor

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,6 +1,6 @@
 const Instagram = require('../lib/Class/instagram');
 const { Module } = require('../lib/plugins');
-const UrlUtil = require('../lib/UrlUtil'); 
+const UrlUtil = require('../lib/UrlUtil');
 
 Module({
   command: 'insta',
@@ -12,12 +12,13 @@ Module({
   const res = await ig.get(match);
   if (!res.files.length || res.files[0].url === 'not found') return await message.send('err');
   for (const file of res.files) {
-  if (file.type === 'mp4') {
-  await message.send({ video: { url: file.url } }, { quoted: message });
-  } else {
-  await message.send({ image: { url: file.url } }, { quoted: message });
+    if (file.type === 'mp4') {
+      await message.send({ video: { url: file.url } }, { quoted: message });
+    } else {
+      await message.send({ image: { url: file.url } }, { quoted: message });
+    }
   }
-}});
+});
 
 Module({
   on: 'text'
@@ -29,10 +30,10 @@ Module({
   const res = await insta.get(ig);
   if (!res.files.length || res.files[0].url === 'not found') return await message.send('err');
   for (const file of res.files) {
-  if (file.type === 'mp4') {
-  await message.send({ video: { url: file.url } }, { quoted: message });
-  } else {
-  await message.send({ image: { url: file.url } }, { quoted: message });
-  }
+    if (file.type === 'mp4') {
+      await message.send({ video: { url: file.url } }, { quoted: message });
+    } else {
+      await message.send({ image: { url: file.url } }, { quoted: message });
+    }
   }
 });
