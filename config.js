c7eaa1170417c6d5e2f7fb550de0b580e0982104
naxor <naxordeve@gmail.com>
2025-07-20 15:45:00 +0200

    naxor

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
     packname: process.env.PACKNAME || 'ɠαɾϝιҽʅɗ',
     author: process.env.AUTHOR || 'ɳαxσɾ',
     SESSION_ID: process.env.SESSION_ID || '',
-    HASTEBIN: process.env.HASTEBIN || '427ab9bad04c076e1d20bfd8d4bae160b6598120246fee71dde98ffb4da39abe79b11890ae4f8c535abb07036a34fff9ad1d83183d71c97b876123069acf5f8e',
+    HASTEBIN: process.env.HASTEBIN || '57575e64f0071399dbffa7b2ec6ac1ed32d3e9167f87893dcd4fdcfc73de970a1348c31b4711d06426298080cf6072e4ff3fd9675af1bb08101ab259e05f1bbe',
     maxFileSize: parseInt(process.env.MAX_FILE_SIZE) || 100 * 1024 * 1024,
     timezone: process.env.TIMEZONE || 'UTC',
     WORK_TYPE: process.env.WORK_TYPE || 'public',
