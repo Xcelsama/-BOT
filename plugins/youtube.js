9e6b8c9b8079e3c2c42baae850e29ef114a21135
naxor <naxordeve@gmail.com>
2025-07-21 11:52:02 +0200

    naxor

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,37 +1,19 @@
 const { Module } = require('../lib/plugins');
-const ytSearch = require('yt-search');
-const downloadMusicAndVideos = require('../lib/ytdl-dlp');
-const tag = require('../lib/Class/metadata');
+const axios = require('axios');
+const ID3 = require('node-id3');
+const yts = require('yt-search');
+const downloadMusicAndVideos = require('../lib/downloadMusicAndVideos');
 
-Module({
-  command: 'song',
-  package: 'downloader',
-  description: 'Download audio from YouTube by URL or search query'
-})(async (message, match) => {
+Module({ command: 'song', package: 'downloader', description: 'Download audio' })(async (message, match) => {
   if (!match) return message.send('Provide a YouTube link or search query');
-  const r = /https:\/\/(www\.youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/shorts\/)([^&\s]+)/;
-  let u = match;
-  let x;
-  if (!r.test(match)) {
-    const s = await ytSearch(match);
-    x = s.videos?.[0];
-    if (!x) return message.send('nfound');
-    u = x.url;
-  }
-
-  const d = await downloadMusicAndVideos(u);
-  if (d.status !== 'success') return message.send(d.message);
-  const id = x?.videoId || (u.match(/([0-9A-Za-z_-]{11})/) || [])[1];
-  const a = d.url;
-  const t = d.title;
-  const n = x?.author.name;
-  const { buffer, thumbUrl } = await tag(a, t, n, id);
-  await message.send({document: buffer,mimetype: 'audio/mpeg',
-    fileName: t + '.mp3',
-    contextInfo: {externalAdReply: {title: t,body: 'Audio',mediaType: 2,thumbnailUrl: thumbUrl,mediaUrl: a,sourceUrl: a,renderLargerThumbnail: false
-      }
-    }
-  }, { quoted: message });
+  const r = /https:\/\/(www\.youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/shorts\/)/.test(match) ? match : (await yts(match)).videos?.[0]?.url;
+  if (!r) return message.send('nfound');
+  const x = await downloadMusicAndVideos(r); if (x.status !== 'success') return message.send(x.message);
+  const a = Buffer.from((await axios.get(x.url, { responseType: 'arraybuffer' })).data);
+  const t = x.url.split('v=')[1]?.slice(0, 11);
+  const i = Buffer.from((await axios.get(`https://i.ytimg.com/vi/${t}/hqdefault.jpg`, { responseType: 'arraybuffer' })).data);
+  const meta = ID3.write({ title: x.title, artist: x.author, album: 'Audio', comment: { text: 'Garfield' }, image: { mime: 'image/jpeg', type: { id: 3, name: 'front cover' }, imageBuffer: i } }, a);
+  await message.send({ document: meta, mimetype: 'audio/mpeg', fileName: `${x.title}.mp3`, contextInfo: { externalAdReply: { title: x.title, body: x.author, mediaType: 2, thumbnailUrl: `https://i.ytimg.com/vi/${t}/hqdefault.jpg`, mediaUrl: 'www.tubidy.mobi', sourceUrl: 'www.tubidy.mobi', renderLargerThumbnail: false } } }, { quoted: message});
 });
 
 /*Module({
