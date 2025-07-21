7fa4fb9fde47e4afbf6af50e3dcd8acb8ad724eb
naxor <naxordeve@gmail.com>
2025-07-21 12:44:57 +0200

    naxor

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,17 +1,66 @@
-const axios = require('axios');
-const ID3 = require('node-id3');
-const ytSearch = require('yt-search');
 const { Module } = require('../lib/plugins');
+const ytSearch = require('yt-search');
+const axios = require('axios');
+const { default: ID3Writer } = require('browser-id3-writer');
 const downloadMusicAndVideos = require('../lib/ytdl-dlp');
 
-const getVid = u => u.match(/(?:v=|youtu\.be\/|\/shorts\/)([\w-]{11})/)?.[1]||null;
-Module({command:'song',package:'downloader',description:'Download audio'})(async(message,match)=>{ 
-if(!match) return message.send('Provide a link or query'); let url=match; if(!/(youtu\.be|youtube\.com)/.test(match)){ 
-const s = await ytSearch(match); const v = s.videos?.[0]; if(!v)return message.send('nothin'); url=v.url;}
-const s = await ytSearch(url),v=s.videos?.[0]||{},vid=v.videoId||getVid(url);if(!vid)return message.send('invalid id_'); const r=await downloadMusicAndVideos(url);if(r.status!=='success')return message.send(r.message);
-const audio=Buffer.from((await axios.get(r.url,{responseType:'arraybuffer'})).data);const img=Buffer.from((await axios.get(`https://i.ytimg.com/vi/${vid}/hqdefault.jpg`,{responseType:'arraybuffer'})).data);const out=ID3.write({title:r.title,artist:v.author?.name||'garfield',album:'Audio',image:{mime:'image/jpeg',type:{id:3,name:'cover'},imageBuffer:img}},audio);
-await message.send({audio:out,mimetype:'audio/mpeg',fileName:r.title+'.mp3',contextInfo:{externalAdReply:{title:r.title,body:v.author?.name||'garfield',mediaType:2,thumbnailUrl:`https://i.ytimg.com/vi/${vid}/hqdefault.jpg`,mediaUrl:'www.tubidy.mobi',sourceUrl:'www.tubidy.mobi',renderLargerThumbnail:false}}},{quoted:message});});
+Module({
+  command: 'song',
+  package: 'downloader'
+})(async (message, match) => {
+  if (!match) return message.send('Send a song name or YouTube link');
 
+  const ytMatch = /(?:youtube\.com\/watch\?v=|youtu\.be\/)([^\s&]+)/.exec(match);
+  let u = match, title = '', author = '', id = '';
+
+  if (!ytMatch) {
+    const s = await ytSearch(match);
+    const v = s.videos?.[0];
+    if (!v) return message.send('Not found');
+    u = v.url;
+    title = v.title;
+    author = v.author.name;
+    id = v.videoId;
+  } else {
+    id = ytMatch[1];
+    const s = await ytSearch({ videoId: id });
+    const v = s.videos?.[0];
+    if (v) {
+      title = v.title;
+      author = v.author.name;
+    }
+  }
+
+  const r = await downloadMusicAndVideos(u);
+  if (!r || r.status !== 'success') return message.send('Download failed');
+
+  const audio = (await axios.get(r.url, { responseType: 'arraybuffer' })).data;
+  const thumb = (await axios.get(`https://i.ytimg.com/vi/${id}/hqdefault.jpg`, { responseType: 'arraybuffer' })).data;
+
+  const writer = new ID3Writer(audio);
+  writer.setFrame('TIT2', title)
+        .setFrame('TPE1', [author])
+        .setFrame('APIC', { type: 3, data: thumb, description: 'Cover' })
+        .addTag();
+  const tagged = Buffer.from(writer.arrayBuffer);
+
+  return await message.send({
+    document: tagged,
+    mimetype: 'audio/mpeg',
+    fileName: title + '.mp3',
+    contextInfo: {
+      externalAdReply: {
+        title,
+        body: author,
+        mediaType: 2,
+        thumbnailUrl: `https://i.ytimg.com/vi/${id}/hqdefault.jpg`,
+        mediaUrl: 'https://tubidy.mobi',
+        sourceUrl: 'https://tubidy.mobi',
+        renderLargerThumbnail: false
+      }
+    }
+  }, { quoted: message });
+});
 /*Module({
   command: 'play',
   package: 'downloader',
